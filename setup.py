004b8d70d128cb3d6751fd7bf9ce548f53776462
Himel Datta <hdutta730@gmail.com>
2026-02-28 22:47:36 +0600

    First set up commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    """This function will return the list of requirements."""
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('/n',"") for req in requirements]
+
+    return requirements
+
+
+
+
+
+setup(
+
+
+
+    name="demo",
+    version="0.1.0",
+    author="Himel",
+    author_email="hdutta730@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    
+    )
